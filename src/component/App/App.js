43addd9aa0361322255215b0748bdb129f2f73d1
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-02-15 17:49:23 -0500

    change and move css style to the seprate file

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,28 +1,14 @@
 import React from "react";
 import Display from "../Display/Display";
 import ButtonGroup from "../ButtonGroup/ButtonGroup";
-const style = {
-  display: "flex",
-  alignItems: "center",
-  jusifyContent: "center",
-  height: "100vh",
-};
+import "./App.css"
+import "./Calculator.css"
 function App() {
   return (
-    <div className="App" style={style}>
-      <div className="Calculator" style={{ margin: "0 auto" }}>
-        <div
-          style={{
-            width: 435,
-            height: 675,
-            backgroundColor: "#9A9999",
-            padding: "20px 30px",
-            borderRadius: 20,
-          }}
-        >
+    <div className="App" >
+      <div className="Calculator">
           <Display />
           <ButtonGroup />
-        </div>
       </div>
     </div>
   );
